afcbf47614dcf3688bfcb22ea34a15a19a32e164
Guilherme Silvestre <lgsilvestre23@gmail.com>
2025-10-31 16:48:05 -0300

    Fix STATIC_URL path in settings.py

--- project/settings.py
+++ project/settings.py
@@ -112,7 +112,7 @@ USE_I18N = True
 USE_TZ = True
 
 # -------------------- ARQUIVOS ESTÁTICOS --------------------
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
