be17954548477fd936acfbe05df656f1db339459
Guilherme Silvestre <lgsilvestre23@gmail.com>
2025-11-03 11:11:50 -0300

    Enable serving media files in development
    Add static file serving for development.

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from noticias.views import registro_usuario
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,3 +16,7 @@ urlpatterns = [
     # Página inicial e rotas da app 'noticias'
     path('', include('noticias.urls')),
 ]
+
+# servir /media/ em desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
