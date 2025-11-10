f317e0bb6245a9f9a25cd30865cc94b6a4025f1f
Felipe <fal@cesar.school>
2025-11-10 10:42:58 -0300

    correção configurações

--- noticias/forms.py
+++ noticias/forms.py
@@ -1,12 +1,12 @@
 # noticias/forms.py
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.validators import UnicodeUsernameValidator
-from django.contrib.auth import authenticate
+from django.contrib.auth.forms import PasswordChangeForm
 from .models import UserProfile
 
 User = get_user_model()
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
@@ -21,7 +21,8 @@ class ProfileForm(forms.ModelForm):
                 "class": "block w-full text-sm text-gray-700"
             }),
         }
-        
+
+
 class UsernameChangeForm(forms.Form):
     username = forms.CharField(
         max_length=150,
@@ -29,14 +30,14 @@ class UsernameChangeForm(forms.Form):
         validators=[UnicodeUsernameValidator()],
         widget=forms.TextInput(attrs={
             "placeholder": "Novo usuário",
-            "class": "w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500"
+            "class": "w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500"
         })
     )
     current_password = forms.CharField(
         label="Senha atual",
         widget=forms.PasswordInput(attrs={
             "placeholder": "Senha atual",
-            "class": "w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500"
+            "class": "w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500"
         })
     )
 
@@ -62,3 +63,27 @@ class UsernameChangeForm(forms.Form):
         self.user.username = self.cleaned_data["username"]
         self.user.save(update_fields=["username"])
         return self.user
+
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # ✅ Estilo de CAIXINHA VISÍVEL:
+        box_style = (
+            "w-full px-3 py-2 rounded-md border border-gray-300 "
+            "focus:outline-none focus:ring-2 focus:ring-red-500"
+        )
+
+        self.fields['old_password'].widget.attrs.update({
+            'class': box_style,
+            'placeholder': 'Sua senha antiga'
+        })
+        self.fields['new_password1'].widget.attrs.update({
+            'class': box_style,
+            'placeholder': 'Sua nova senha'
+        })
+        self.fields['new_password2'].widget.attrs.update({
+            'class': box_style,
+            'placeholder': 'Confirme a nova senha'
+        })
