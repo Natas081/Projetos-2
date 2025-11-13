21f7c534dfcbfa77ee4936fc9b98e0dabc3aaad9
Guilherme <lgsilvestre23@gmail.com>
2025-11-13 12:02:27 -0300

    Alterando validação de senhas

--- project/settings.py
+++ project/settings.py
@@ -98,10 +98,10 @@ if 'DATABASE_URL' in os.environ:
 
 # -------------------- VALIDAÇÃO DE SENHA --------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 4, } },
 ]
 
 # -------------------- IDIOMA E FUSO HORÁRIO --------------------
